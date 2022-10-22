5424a3552f4342382e8692fddf904a06acf4835a
mikhail <mikhail.kuptsov.03@mail.ru>
2022-10-22 09:37:17 +0300

    первое изменение

---
+++ src/task_1_22_10_2022.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class task_1_22_10_2022 {
+    public static void main(String[] args) {
+        Scanner firstScanner=new Scanner(System.in);
+        System.out.println("введите размер первого массива:");
+        int[] firstArray=new int[firstScanner.nextInt()];
+
+        for (int i = 0; i <firstArray.length ; i++) {
+            firstArray[i]= firstScanner.nextInt();
+        }
+
+    }
+}
